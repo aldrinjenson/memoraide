593968dc3532de3ba182061736828243dce6372d
Aldrin Jenson <aldrinjenson@gmail.com>
2022-07-06 19:57:07 +0530

    add base files

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:memoria/screens/authenticate/authenticate.dart';
+import 'package:memoria/screens/home/home.dart';
+
+class Wrapper extends StatefulWidget {
+  const Wrapper({Key? key}) : super(key: key);
+
+  @override
+  State<Wrapper> createState() => _WrapperState();
+}
+
+class _WrapperState extends State<Wrapper> {
+  @override
+  Widget build(BuildContext context) {
+    // replace with proper logic later
+    bool isLoggedIn = 1 > 2;
+    return isLoggedIn ? Home() : Authenticate();
+  }
+}
