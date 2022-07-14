ddb6fa69012a9ef6041b438ad68d8d505273b6c4
Aldrin Jenson <aldrinjenson@gmail.com>
2022-07-14 14:49:29 +0530

    Add firebase config

--- lib/widgets/mode_select_card.dart
+++ lib/widgets/mode_select_card.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: must_be_immutable
 
 import 'package:flutter/material.dart';
-import 'package:memoria/screens/home/face_profile.dart';
 import 'package:memoria/screens/home/face_search.dart';
 
 class ModeSelectCard extends StatelessWidget {
